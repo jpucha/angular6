d26543780f3abd7bf1dc7361b1f1d231aca7e3ff
jpucha <jen_liliana86@hotmail.com>
2019-04-14 18:31:58 -0500

    configuracion de modulo

--- my-first-angular-app/src/app/app.module.ts
+++ my-first-angular-app/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApplicationInfoComponent } from './application-info/application-info.component';
 import { LoginModule } from './login/login.module';
@@ -13,10 +12,10 @@ import { LoginModule } from './login/login.module';
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
     LoginModule
   ],
   providers: [],
-  bootstrap: [ApplicationInfoComponent]
+  exports: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
